d34a64adc1408c9b58f64704b5277d66018ee06c
udenwani nonso <nolly190@gmail.com>
2019-10-24 01:32:01 +0100

    Initial Commit

---
+++ Lonzec Txt Converter/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Lonzec_Txt_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbExcel = new System.Windows.Forms.RadioButton();
+            this.rbPdf = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.button1.Location = new System.Drawing.Point(49, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Document Converter v1";
+            // 
+            // rbExcel
+            // 
+            this.rbExcel.AutoSize = true;
+            this.rbExcel.Location = new System.Drawing.Point(48, 18);
+            this.rbExcel.Name = "rbExcel";
+            this.rbExcel.Size = new System.Drawing.Size(107, 17);
+            this.rbExcel.TabIndex = 2;
+            this.rbExcel.TabStop = true;
+            this.rbExcel.Text = "Convert To Excel";
+            this.rbExcel.UseVisualStyleBackColor = true;
+            // 
+            // rbPdf
+            // 
+            this.rbPdf.AutoSize = true;
+            this.rbPdf.Location = new System.Drawing.Point(48, 53);
+            this.rbPdf.Name = "rbPdf";
+            this.rbPdf.Size = new System.Drawing.Size(93, 17);
+            this.rbPdf.TabIndex = 3;
+            this.rbPdf.TabStop = true;
+            this.rbPdf.Text = "Convert to Pdf";
+            this.rbPdf.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.rbPdf);
+            this.panel1.Controls.Add(this.rbExcel);
+            this.panel1.Location = new System.Drawing.Point(239, 121);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(307, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.BackColor = System.Drawing.Color.Transparent;
+            this.lblFilePath.Font = new System.Drawing.Font("Century Gothic", 10.75F);
+            this.lblFilePath.Location = new System.Drawing.Point(177, 75);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(0, 20);
+            this.lblFilePath.TabIndex = 5;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnConvert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.btnConvert.Location = new System.Drawing.Point(267, 262);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(113, 47);
+            this.btnConvert.TabIndex = 6;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Lonzec_Txt_Converter.Properties.Resources.asset6___Copy;
+            this.ClientSize = new System.Drawing.Size(683, 342);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbExcel;
+        private System.Windows.Forms.RadioButton rbPdf;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ Lonzec Txt Converter/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text;
+using Bytescout.Spreadsheet;
+using iTextSharp.text.pdf;
+
+namespace Lonzec_Txt_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private  void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var fileDialog = new OpenFileDialog() { Filter = "Text File |*.txt", Multiselect = false })
+                {
+                    if (fileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                       
+                        lblFilePath.Text = fileDialog.FileName;
+                      
+                          var ext = System.IO.Path.GetExtension(fileDialog.FileName);
+                          if (ext!=".txt")
+                          {
+                              MessageBox.Show("Select a txt file");
+                              lblFilePath.Text = "";
+                          }
+
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rbExcel.Checked = true;
+        }
+
+        private async void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+                string readString = null;
+                using (
+                    var streamReader = new StreamReader(lblFilePath.Text))
+                {
+                    readString = await streamReader.ReadLineAsync();
+                    var newList = new List<Task<string>>();
+                    while (!streamReader.EndOfStream)
+                    {
+                        newList.Add(streamReader.ReadLineAsync());
+                    }
+                    var getExactUrl = System.IO.Path.GetDirectoryName(lblFilePath.Text);
+
+                    if (rbPdf.Checked)
+                    {
+                        var size = newList[0].Result.Split('|');
+                        Document pdfDocument = new Document();
+                        PdfPTable pdfTable = new PdfPTable(size.Length);
+                        int num = 1;
+                        foreach (Task<string> item in newList)
+                        {
+                            size = item.Result.Split('|');
+
+                            for (int i = 0; i < size.Length; i++)
+                            {
+
+                                pdfTable.AddCell(size[i] ?? " ");
+
+                            }
+
+                            num++;
+                        }
+                        if (File.Exists(getExactUrl + "ConvertedPdf.pdf"))
+                        {
+                            File.Delete(getExactUrl + "ConvertedPdf.pdf");
+                        }
+                        using (var fileStream = new FileStream(getExactUrl + "ConvertedPdf.pdf", FileMode.Create))
+                        {
+
+
+                            PdfWriter.GetInstance(pdfDocument, fileStream);
+                            pdfDocument.Open();
+                            pdfDocument.Add(pdfTable);
+                            pdfDocument.Close();
+                            fileStream.Close();
+                            Process.Start(getExactUrl + "ConvertedPdf.pdf");
+                        }
+                    }
+                    else
+                    {
+                        var newDocument = new Spreadsheet();
+                        var sheet = newDocument.Workbook.Worksheets.Add("Converted Excel");
+                        int num = 1;
+                        foreach (Task<string> item in newList)
+                        {
+                            var size = item.Result.Split('|');
+
+                            for (int i = 0; i < size.Length; i++)
+                            {
+
+                                sheet.Cell(num, i).Value = size[i] ?? " ";
+
+                            }
+
+                            num++;
+                        }
+
+                        if (File.Exists(getExactUrl + "ConvertedExcel.xlsx"))
+                        {
+                            File.Delete(getExactUrl + "ConvertedExcel.xlsx");
+                        }
+                        newDocument.SaveAs(getExactUrl + "ConvertedExcel.xlsx");
+                        newDocument.Close();
+                        Process.Start(getExactUrl + "ConvertedExcel.xlsx");
+                    }
+                }
+
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+            
+        }
+    }
+}
